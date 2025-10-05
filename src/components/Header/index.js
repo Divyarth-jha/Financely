06d20ed855573e70572b0d9d2c197e843ec65be9
Divyarth-jha <divyarthjha@gmail.com>
2025-10-05 15:48:20 +0530

    fixing header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,10 +5,10 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { signOut } from 'firebase/auth';
-import userImg from "../../assets/user.svg"
+import userImg from "../../assets/user.svg";
 
 const Header = () => {
-  const navigate = useNavigate();  // <-- add parentheses
+  const navigate = useNavigate();
   const [user, loading] = useAuthState(auth);
 
   useEffect(() => {
@@ -18,31 +18,43 @@ const Header = () => {
   }, [user, loading, navigate]);
 
   function logoutfnc() {
-    try{
-      signOut(auth).then(()=>{
-        //sign-out
-        toast.success("sign-Out successful")
-        navigate("/")
-      }).catch((error)=>{
-        //error
-        toast.error(e.message)
-      })
-    }catch(e){
-      toast.error(e.message)
+    try {
+      signOut(auth)
+        .then(() => {
+          toast.success("Sign-out successful");
+          navigate("/");
+        })
+        .catch((error) => {
+          toast.error(error.message);
+        });
+    } catch (error) {
+      toast.error(error.message);
     }
   }
-  
+
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <p className="logo">Financely</p>
-      {user && <div style={{
-        display:"flex",
-        alignItems:"center",
-        gap:"1.5rem"}}> 
-        <img src={user.photoURL?user.photoURL:userImg} style={{width:"2rem",height:"2rem"}}/>
-        <p onClick={logoutfnc} className='logo link'>Logout</p></div>}
+      {user && (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "1.5rem",
+          }}
+        >
+          <img
+            src={user.photoURL ? user.photoURL : userImg}
+            style={{ width: "2rem", height: "2rem" }}
+            alt="User"
+          />
+          <p onClick={logoutfnc} className="logo link">
+            Logout
+          </p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Header;
