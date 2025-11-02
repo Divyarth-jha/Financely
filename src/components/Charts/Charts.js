fbccea1d518a4bd1c722ca2498e52f497716f615
divyarth jha <166954164+Divyarth-jha@users.noreply.github.com>
2025-11-02 13:25:29 +0530

    Update Charts.js

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -14,18 +14,17 @@ function Chart({ shortedTransactions = [] }) {
       amount: transaction.amount,
     }));
 
-    let newSpendings = [
-{ tag: "food", amount: 0 },
-{ tag: "education", amount: 0 },
-{ tag: "office", amount: 0 },]
-pieData.forEach((item) =>{
-if (item.tag == "food") {
-newSpendings[0].amount += item.amount;
-} else if (item.tag == "education") {
-newSpendings[1].amount += item.amount;
-} else {
-newSpendings[2].amount += item.amount;
-}
+  let newSpendings = [
+  { tag: "income", amount: 0 },
+  { tag: "expense", amount: 0 },
+];
+
+shortedTransactions.forEach((item) => {
+  if (item.type === "income") {
+    newSpendings[0].amount += item.amount;
+  } else if (item.type === "expense") {
+    newSpendings[1].amount += item.amount;
+  }
 });
 
   const lineConfig = {
